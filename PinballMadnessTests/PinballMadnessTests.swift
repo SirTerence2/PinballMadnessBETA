2e1903de382a9961dc230068268fa433091bfacc
Mahmmad <mmahmood6136@gmail.com>
2025-08-06 20:58:34 -0700

    fixing my oopsie

---
+++ PinballMadnessTests/PinballMadnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  PinballMadnessTests.swift
+//  PinballMadnessTests
+//
+//  Created by Muhammad Mahmood on 7/25/25.
+//
+
+import Testing
+@testable import PinballMadness
+
+struct PinballMadnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
